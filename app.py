ad48c2a26c547264ddcc6b5da04dace1564aa81e
devinawongso <155947907+devinawongso@users.noreply.github.com>
2024-01-09 01:03:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,34 +11,7 @@ import os
 This GPT-powered chatbot can retrieve test questions and converse with the students to review their mistakes in the test.
 Demo Version: Currently only support Alpha ELA STAAR G5.2017 test
 """
-
-load_dotenv()
-
-def sidebar():
-    with st.sidebar:
-        st.markdown(
-            "## How to use\n"
-            "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) below🔑\n"  # noqa: E501
-            "2. Converse with the bot to review your mistakes\n"
-        )
-        api_key_input = st.text_input(
-            "OpenAI API Key",
-            type="password",
-            placeholder="Paste your OpenAI API key here (sk-...)",
-            help="You can get your API key from https://platform.openai.com/account/api-keys.",  # noqa: E501
-            value=os.environ.get("OPENAI_API_KEY", None)
-            or st.session_state.get("OPENAI_API_KEY", ""),
-        )
-
-        st.session_state["OPENAI_API_KEY"] = api_key_input
-
-sidebar()
-openai_api_key = st.session_state.get("OPENAI_API_KEY")
-if not openai_api_key:
-    st.warning(
-        "Enter your OpenAI API key in the sidebar. You can get a key at"
-        " https://platform.openai.com/account/api-keys."
-    )
+openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
     
 if "messages" not in st.session_state.keys(): # Initialize the chat message history
     st.session_state.messages = [
